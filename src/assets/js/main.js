1dd14e3b13801eadb99d664456df43d0bbffa4a5
Albert Bronsky <bronsky.albert@gmail.com>
2019-06-04 07:28:43 +0300

    wip: country search

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,20 +1,39 @@
-// let country(name, capital, continent, government, population, area) = {
-//     name: name;
-//     capital: capital;
-//     continent: continent;
-//     government: government;
-//     population: population;
-//     area: area;
-// }
-
 // Check input
 var lastValue = '';
 $("#textbox").on('input', function () {
     if ($(this).val() != lastValue) {
         lastValue = $(this).val();
-        console.log(lastValue)
+        country_search(lastValue);
     }
 });
+
+function country_search(term) {
+    // class Country {
+    //     name: name;
+    //     capital: capital;
+    //     continent: continent;
+    //     government: government;
+    //     population: population;
+    //     area: area;
+    // }
+
+    class Country {
+        constructor(name, capital, continent, government, population, area) {
+            this.name = name;
+            this.capital = capital;
+            this.continent = continent;
+            this.government = government;
+            this.population = population;
+            this.area = area;
+
+            this.getName = function () {
+                // return "User's name: " + this.firstName + " " + this.lastName;
+            }
+        }
+    }
+
+    var user001 = new Country("John", "Smith", 1985);
+}
 
 $.getJSON("assets/json/db.json", function (data) {
     // console.log(data.find(x => x.country === 'Aruba').population);
