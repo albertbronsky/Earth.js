ccb79ec914114e8b7030cb46ae1ff71c4e640bda
Albert Bronsky <bronsky.albert@gmail.com>
2019-06-05 19:10:08 +0300

    basic search for countries

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,52 +1,99 @@
-// Check input
-var lastValue = '';
-$("#textbox").on('input', function () {
-    if ($(this).val() != lastValue) {
-        lastValue = $(this).val();
-        country_search(lastValue);
-    }
+$(document).ready(function() {
+  get_json();
+  input_check();
 });
 
-function country_search(term) {
-    // class Country {
-    //     name: name;
-    //     capital: capital;
-    //     continent: continent;
-    //     government: government;
-    //     population: population;
-    //     area: area;
-    // }
+// function object_check(obj) {
+//   if (typeof obj === "object" && obj !== null) {
+//     return true;
+//   }
+// }
 
-    class Country {
-        constructor(name, capital, continent, government, population, area) {
-            this.name = name;
-            this.capital = capital;
-            this.continent = continent;
-            this.government = government;
-            this.population = population;
-            this.area = area;
+// function country_search(term, data) {
+//   country_data = data.find(e => e.country === term);
+//   if (object_check(country_data)) {
+//     console.log(country_data.city);
+//     // new Country()
+//   }
 
-            this.getName = function () {
-                // return "User's name: " + this.firstName + " " + this.lastName;
-            }
-        }
-    }
+//   class Country {
+//     constructor(name, capital, continent, government, population, area) {
+//       this.name = name;
+//       this.capital = capital;
+//       this.continent = continent;
+//       this.government = government;
+//       this.population = population;
+//       this.area = area;
 
-    var user001 = new Country("test", "Smith", 1985);
+//       this.getName = function() {
+//         return "Country name: " + this.name;
+//       };
+//     }
+//   }
+
+//   // var user001 = new Country("test", "Smith", 1985);
+// }
+
+// class Search {
+//   constructor(name, capital, continent, government, population, area) {
+//     this.name = name;
+//     this.capital = capital;
+//     this.continent = continent;
+//     this.government = government;
+//     this.population = population;
+//     this.area = area;
+//     // country_data = data.find(e => e.country === term)
+
+//     this.getName = function() {
+//       return "Country name: " + this.name;
+//     };
+//   }
+// }
+
+// valid categories: countries,
+var json;
+
+function get_json() {
+  $.getJSON(`assets/json/db.json`, function(data) {
+    json = data;
+  });
 }
 
-$.getJSON("assets/json/db.json", function (data) {
-    // console.log(data.find(x => x.country === 'Aruba').population);
-    // console.log(items[0]);
-    for (i in data) {
-        console.log(i)
+function toTitleCase(phrase) {
+  return phrase
+    .toLowerCase()
+    .split(" ")
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
+
+function input_check() {
+  const category = "countries";
+
+  $("#gt").on("input", function() {
+    value = toTitleCase($(this).val());
+    if (value.length > 0) {
+      render_suggestions(value);
+      // get_json(value, category);
+    } else {
+      $("#search-results").empty();
     }
-    // $.each( data, function( key, val ) {
-    //     items.push( "<li id='" + key + "'>" + val + "</li>" );
-    // });
-    //
-    // $( "<ul/>", {
-    //     "class": "my-new-list",
-    //     html: items.join( "" )
-    // }).appendTo( "body" );
-});
+  });
+}
+
+function render_suggestions(term) {
+  filtered = json.Countries.filter(e => e.country.startsWith(term));
+
+  let items = [];
+
+  for (item in filtered) {
+    items.push("<li id='" + item + "'>" + filtered[item].country + "</li>");
+  }
+
+  $("#search-results").html(
+    $("<ul/>", {
+      class: "my-new-list",
+      html: items.join("")
+    })
+  );
+}
